e1d961833c386dc480f68e3654cc6a5164f91123
erico.paschoito <erico.paschoito@b2wdigital.com>
2016-08-04 09:07:34 -0300

    Caixa de Mensagem para receber o nome do aluno

--- Saudacao.java
+++ Saudacao.java
@@ -1,6 +1,17 @@
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+
 class Saudacao{
   
   public static void main(String args[]){
-    System.out.println(args[0]+" Bem vindo ao curso");
+    System.out.print("Digite o nome do aluno: ");
+    
+    Scanner entrada = new Scanner(System.in);
+    
+    // Aqui criamos um objeto que irá receber o nome do aluno
+    //String nome = entrada.nextLine();
+    String nome = JOptionPane.showInputDialog("Digite o nome do aluno");
+    System.out.println(nome+" Bem vindo ao curso");
+
   }
 }
